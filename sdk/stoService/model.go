47eed3775c0b4d5a12326a1e151617917b002540
Vladislav Zalutsky <vladislav.zalutsky@fasunok.com>
2023-06-27 16:10:37 +0300

    Change sto model

--- sdk/stoService/model.go
+++ sdk/stoService/model.go
@@ -7,19 +7,32 @@ type Sto struct {
 }
 
 type CategoryWithServices struct {
-	Id               string          `json:"CategoryID"`
-	Alias            string          `json:"CategoryAlias"`
-	Name             string          `json:"CategoryName"`
-	ExcludedServices []string        `json:"ExcludedServices"`
-	IncludedServices []string        `json:"IncludedServices"`
-	IsServicesAll    bool            `json:"IsServicesAll"`
-	CustomService    []CustomService `json:"ServicesCustom"`
+	Id               string          `json:"category_id"`
+	Alias            string          `json:"category_alias"`
+	Name             string          `json:"category_name"`
+	ExcludedServices []string        `json:"excluded_services"`
+	IncludedServices []string        `json:"included_services"`
+	IsServicesAll    bool            `json:"is_services_all"`
+	CustomService    []CustomService `json:"services_custom"`
 }
 
 type CustomService struct {
-	CustomId           string `json:"CustomId"`
-	Name               string `json:"Name"`
-	ReferenceServiceId string `json:"ReferenceServiceId"`
-	Id                 string `json:"CategoryId"`
-	ChildServices      string `json:"ChildServices"`
+	CustomId           string   `json:"custom_id"`
+	Name               string   `json:"name"`
+	ReferenceServiceId string   `json:"reference_service_id"`
+	Id                 string   `json:"category_id"`
+	ChildServices      string   `json:"child_services"`
+	Price              Price    `json:"price"`
+	Duration           Duration `json:"duration"`
+}
+
+type Price struct {
+	IsFixed    bool    `json:"is_fixed"`
+	FixedPrice float64 `json:"fixed_price"`
+	HourPrice  float64 `json:"hour_price"`
+}
+
+type Duration struct {
+	Calendar int64 `json:"calendar"`
+	Default  int64 `json:"default"`
 }
